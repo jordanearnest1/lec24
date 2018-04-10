0136e4e62c50f746ff0ba509f20bcf2e25df41f7
Jordan A. Earnest <jordanearnest1@gmail.com>
2018-04-10 15:08:19 -0400

    bball and football are working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -13,9 +13,55 @@ def index():
         </ul>
     '''
 
-@app.route('/bball')
-def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+## the default method is that it excepts "get" (like get.requests) and nothing else
+
+
+# def bball():
+#     return render_template("seasons.html", seasons=model.get_bball_seasons())
+
+
+ ##we can the route to accept two different kinds (will be a post if they submit the form)
+@app.route('/bball', methods=['GET', 'POST'])  
+def bball():  
+    # need to be able to post on the url, so adding that in
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+        
+    return render_template("seasons.html", seasons=seasons)
+
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == "POST":
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+    else:
+        firstname = ''
+        lastname = ''
+
+    return render_template("hello.html", firstname= firstname, lastname= lastname)
+
+
+@app.route('/fball', methods=['GET', 'POST'])  
+def fball():  
+    # need to be able to post on the url, so adding that in
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_football_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_football_seasons()
+        
+    return render_template("seasons.html", seasons=seasons)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    model.init_bball()
+    # print(model.get_bball_seasons())
+    model.init_football()
+    app.run(debug=True, port=8080)
--- model.py
+++ model.py
@@ -1,13 +1,94 @@
 #model.py
+from flask import Flask, render_template
+
 import csv
+
+## model is the only thing that should know where the data is stored
+
+app = Flask(__name__)
 
 BB_FILE_NAME = 'umbball.csv'
 
+FB_FILE_NAME = 'umfootball.csv'
+
+
 bb_seasons = []
+fb_seasons = []
 
 def init_bball(csv_file_name=BB_FILE_NAME):
-    pass
+
+    with open(BB_FILE_NAME) as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader) # throw away headers
+        global bb_seasons
+# bb_seasons = [] # reset, start clean
+        ## convert the strings into integers and save them
+        bb_seasons = [] # reset, start clean
+        for r in reader:
+            r[3] = int(r[3])
+            r[4] = int(r[4])
+            r[5] = float(r[5])
+            bb_seasons.append(r)
+
+
 
 def get_bball_seasons(sortby='year', sortorder='desc'):
     global bb_seasons
+
+    if sortby == 'year':
+            sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+    
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
+##returning the item at index sort col of row
     return bb_seasons
+
+
+############## getting football ###########
+
+def init_football(csv_file_name=FB_FILE_NAME):
+
+    with open(FB_FILE_NAME) as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader) # throw away headers
+        global fb_seasons
+# bb_seasons = [] # reset, start clean
+        ## convert the strings into integers and save them
+        fb_seasons = [] # reset, start clean
+        for r in reader:
+            r[3] = int(r[3])
+            r[4] = int(r[4])
+            r[5] = float(r[5])
+            fb_seasons.append(r)
+
+
+
+def get_football_seasons(sortby='year', sortorder='desc'):
+    global fb_seasons
+
+    if sortby == 'year':
+            sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+    
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(fb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
+##returning the item at index sort col of row
+    return fb_seasons
+
+
+
